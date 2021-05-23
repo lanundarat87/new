3ecd44d9ab2f9a938a1b7512ed754b23b66cf9e1
lanundarat87 <lanundarat87@users.noreply.github.com>
2021-05-24 02:29:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,20 +6,6 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 echo "OpenVZ is not supported"
 exit 1
 fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/lanundarat87/new/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-rm -f setup.sh
-exit 0
-fi
 mkdir /var/lib/premium-script;
 echo "Enter the VPS Subdomain Hostname, if not available, please click Enter"
 read -p "Hostname / Domain: " host
