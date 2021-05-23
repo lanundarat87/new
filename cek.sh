7e4dfef25b64e1c85f0f445b5bc4083b524d203e
lanundarat87 <lanundarat87@users.noreply.github.com>
2021-05-24 00:41:10 +0800

    Update cek.sh

--- cek.sh
+++ cek.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script by : LostServer
+# Script by : Badboy
 clear
 echo " "
 echo " "
@@ -72,5 +72,5 @@ echo " Multi Login = kill "
 echo " Cara pakai : kill [nombor PID] "
 echo "-------------------------------------"
 
-echo " By LostServer"
+echo " By Badboy"
 echo "";
