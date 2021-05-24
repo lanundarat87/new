993b6566d6a88f641cd1b2e9b77692e418bd5596
lanundarat87 <lanundarat87@users.noreply.github.com>
2021-05-24 21:34:10 +0800

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+reset
+echo -e "\e[1;33m===============================================================";
+echo -e "\e[1;36m                    AutoScriptVPS by  Badboy                   ";
+echo -e "\e[1;36m                           Redfox VPN                          ";
+echo -e "\e[1;33m===============================================================";
 read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
 IP=$(wget -qO- icanhazip.com);
-echo Script AutoCreate Akun SSH dan OpenVPN by Badboy
 sleep 1
 echo Ping Host
 echo Cek Hak Akses...
@@ -22,21 +25,23 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Terimakasih Telah Menggunakan Layanan Kami"
+echo -e "\e[1;33m==============================================================="
+echo -e "\e[1;36m                    AutoScriptVPS by  Badboy                   "
+echo -e "\e[1;36m                           Redfox VPN                          "
+echo -e "\e[1;33m==============================================================="
 echo -e "Informasi Akun SSH & OpenVPN"
-echo -e "Username       : $Login "
-echo -e "Password       : $Pass"
-echo -e "==============================="
-echo -e "IP Server      : $IP"
-echo -e "Host           : $domain"
-echo -e "OpenSSH        : 22"
-echo -e "Dropbear       : 143, 109"
-echo -e "SSL/TLS        : 443"
-echo -e "Port Squid     : 3128, 8080 (limit to IP SSH)" 
-echo -e "OpenVPN        : TCP 1194 http://$IP:81/client-tcp-1194.ovpn"
-echo -e "OpenVPN        : UDP 2200 http://$IP:81/client-udp-2200.ovpn"
-echo -e "OpenVPN        : SSL 992 http://$IP:81/client-tcp-ssl.ovpn"
-echo -e "badvpn         : 7100, 7200, 7300"
-echo -e "==============================="
-echo -e "Aktif Sampai   : $exp"
-echo -e "Script by Badboy"
+echo -e "\e[1;32mUsername       :\e[1;34m $Login "
+echo -e "\e[1;32mPassword       :\e[1;34m $Pass"
+echo -e "\e[1;32mAktif Sampai   :\e[1;34m $exp"
+echo -e "\e[1;33m==============================="
+echo -e "\e[1;32mIP Server      :\e[1;34m $IP"
+echo -e "\e[1;32mHost           :\e[1;34m $domain"
+echo -e "\e[1;32mOpenSSH        :\e[1;34m 22"
+echo -e "\e[1;32mDropbear       :\e[1;34m 143, 109"
+echo -e "\e[1;32mSSL/TLS        :\e[1;34m 443"
+echo -e "\e[1;32mPort Squid     :\e[1;34m 3128, 8080 (limit to IP SSH)" 
+echo -e "\e[1;32mOpenVPN        :\e[1;34m TCP 1194 http://$IP:81/client-tcp-1194.ovpn"
+echo -e "\e[1;32mOpenVPN        :\e[1;34m UDP 2200 http://$IP:81/client-udp-2200.ovpn"
+echo -e "\e[1;32mOpenVPN        :\e[1;34m SSL 992 http://$IP:81/client-tcp-ssl.ovpn"
+echo -e "\e[1;32mbadvpn         :\e[1;34m 7100, 7200, 7300"
+echo -e "\e[1;33m==============================="
