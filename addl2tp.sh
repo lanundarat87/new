4729df4f42527f65c0046ef50126a66bb5f73edb
lanundarat87 <lanundarat87@users.noreply.github.com>
2021-05-24 20:54:28 +0800

    Update addl2tp.sh

--- addl2tp.sh
+++ addl2tp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+reset
+echo -e "\e[1;33m==============================================================="
+echo -e "\e[1;36m                    AutoScriptVPS by  Badboy                   "
+echo -e "\e[1;36m                           Redfox VPN                          "
+echo -e "\e[1;33m==============================================================="
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 PUBLIC_IP=$(wget -qO- ipv4.icanhazip.com);
